fa600160d033fc728eca0f2a273536fedb1cdcf4
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-02-03 09:27:01 -0500

    Add dependency injection for repository

---
+++ frontend/app/PhotodbModule.scala
@@ -0,0 +1,10 @@
+import cats.Id
+import com.google.inject.AbstractModule
+import com.jmarin.photodb.backend.repositories.algebras.PictureRepository
+import com.jmarin.photodb.backend.repositories.interpreters.inmemory.InMemoryPictureRepository
+
+class PhotodbModule extends AbstractModule {
+  override def configure(): Unit = {
+    bind(classOf[PictureRepository[Id, List]]).toInstance(InMemoryPictureRepository)
+  }
+}
